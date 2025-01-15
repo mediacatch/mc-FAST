9fa4378db406f99a9cf47a4f6aaf1e0d04d69174
rocketsfallonrocketfalls <serdartunckol547@gmail.com>
2025-01-15 14:40:36 +0100

    still working on C++

--- FAST/compile.py
+++ FAST/compile.py
@@ -2,8 +2,12 @@ import os
 import subprocess
 
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+
 def run_compile_sh():
-    run_cmd = "sh FAST/compile.sh"
+    # run_cmd = "sh FAST/compile.sh"
+    run_cmd = "sh " + current_dir + "/compile.sh"
     print(run_cmd)
     p = subprocess.Popen(run_cmd, shell=True, stderr=subprocess.STDOUT)
     p.wait()
